f1b43c11d507d1980a856686c0be1de327d2a9e1
gadjetron <gi87365@yandex.ru>
2020-10-09 22:09:58 +0300

    Add: web screen object example

---
+++ web_apis/web_api_screen.js
@@ -0,0 +1,8 @@
+console.log('Screen object: ', window.screen)
+
+// screen.orientation.lock('landscape-primary')
+// screen.orientation.unlock('landscape-primary')
+
+// screen.orientation.onchange = function (event) {
+//   console.log('Screen orientation was changed!')
+// }
